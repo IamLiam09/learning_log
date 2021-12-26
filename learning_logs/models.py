2443850a5eba821cbad1a8c55111e21b224da102
William <princewill835@gmail.com>
2021-12-26 00:05:03 -0800

    Ready for deployment to heroku.

---
+++ learning_logs/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Topic(models.Model):
+    """A topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        """return a string representation of the model"""
+        return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = 'entries'
+    def __str__(self):
+        """"Return a string representation of the model."""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
